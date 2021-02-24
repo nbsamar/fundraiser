cc4a82c7a5590b84e7c2f6a39e387058366dff0c
Nayab Samar <nbsamar@gmail.com>
2021-02-24 19:07:03 +0530

    adds score for approved campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,12 @@
 class Campaign < ApplicationRecord
   belongs_to :user
   enum status: %i[drafted approved rejected]
+
+  # after_create :add_campaign_points
+
+  def add_campaign_points
+    unless self.user.campaigns.approved.empty?
+
+    end
+  end
 end
