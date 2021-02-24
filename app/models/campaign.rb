b7156fd40cd4b80781ef74e450b4e4599a579ecd
Nayab Samar <nbsamar@gmail.com>
2021-02-24 21:03:14 +0530

    adds method to auto credit score for new campaigns, and calculate credibilty score for a user

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,11 +2,13 @@ class Campaign < ApplicationRecord
   belongs_to :user
   enum status: %i[drafted approved rejected]
 
-  # after_create :add_campaign_points
+  after_create :add_campaign_points
 
   def add_campaign_points
-    unless self.user.campaigns.approved.empty?
-
+    user = self.user
+    profile = Profile.find_by(name: 'Through Campaign')
+    unless user.campaigns.approved.empty?
+      ProfileUser.create(user_id: user.id, profile_id: profile.id)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :campaigns
   has_many :profile_users
   has_many :profiles, through: :profile_users
+
+  def credibility_score
+    t = 0
+    self.profiles.map do |p|
+      t += p.points
+    end
+    t
+  end
 end
